1144db2e8fe39425d00fcd817ab9482546c4fb9c
fzihao1999 <fengzihaogl@outlook.com>
2020-06-12 02:46:49 +0800

    teacher

--- src/fuck/Change_Teacher.java
+++ src/fuck/Change_Teacher.java
@@ -20,12 +20,10 @@ import java.awt.event.ActionEvent;
 public class Change_Teacher {
 
   private JFrame frame;
-  private JTextField textField_courseid;
-  private JTextField textField_classid;
-  private JTextField textField_semester;
+  private JTextField textField_id;
+  private JTextField textField_position;
+  private JTextField textField_tel;
   private JTextField textField;
-  private JLabel label_2;
-  private JTextField textField_teacherid;
 
   /**
    * Launch the application.
@@ -55,61 +53,69 @@ public class Change_Teacher {
    */
   private void initialize() {
     frame = new JFrame();
-    frame.setBounds(100, 100, 429, 389);
+    frame.setBounds(100, 100, 429, 347);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setLayout(null);
-    frame.setTitle("修改任课教师");
+    frame.setTitle("修改教师信息");
     
-    textField_courseid = new JTextField();
-    textField_courseid.setBounds(215, 40, 108, 32);
-    frame.getContentPane().add(textField_courseid);
-    textField_courseid.setColumns(10);
+    textField_id = new JTextField();
+    textField_id.setBounds(215, 40, 108, 32);
+    frame.getContentPane().add(textField_id);
+    textField_id.setColumns(10);
     
-    JLabel lblNewLabel = new JLabel("课程编号：");
+    JLabel lblNewLabel = new JLabel("教师编号：");
     lblNewLabel.setFont(new Font("微软雅黑", Font.PLAIN, 18));
     lblNewLabel.setBounds(87, 40, 115, 32);
     frame.getContentPane().add(lblNewLabel);
     
-    JLabel label = new JLabel("修改后班级编号：");
+    JLabel label = new JLabel("修改教师职称：");
     label.setFont(new Font("微软雅黑", Font.PLAIN, 18));
-    label.setBounds(68, 90, 144, 32);
+    label.setBounds(78, 90, 144, 32);
     frame.getContentPane().add(label);
     
-    textField_classid = new JTextField();
-    textField_classid.setColumns(10);
-    textField_classid.setBounds(215, 90, 108, 32);
-    frame.getContentPane().add(textField_classid);
+    textField_position = new JTextField();
+    textField_position.setColumns(10);
+    textField_position.setBounds(215, 90, 108, 32);
+    frame.getContentPane().add(textField_position);
     
-    JLabel label_1 = new JLabel("修改后开课学期：\r\n");
+    JLabel label_1 = new JLabel("修改教师电话：");
     label_1.setFont(new Font("微软雅黑", Font.PLAIN, 18));
-    label_1.setBounds(68, 140, 144, 32);
+    label_1.setBounds(78, 137, 144, 32);
     frame.getContentPane().add(label_1);
     
-    textField_semester = new JTextField();
-    textField_semester.setColumns(10);
-    textField_semester.setBounds(215, 140, 108, 32);
-    frame.getContentPane().add(textField_semester);
+    textField_tel = new JTextField();
+    textField_tel.setColumns(10);
+    textField_tel.setBounds(215, 140, 108, 32);
+    frame.getContentPane().add(textField_tel);
     
     JButton button = new JButton("确认修改");
     button.setFont(new Font("微软雅黑", Font.PLAIN, 18));
     button.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         try {
-          String courseid = textField_courseid.getText();
-          String classid = textField_classid.getText();
-          String semester = textField_semester.getText();
-          String teacherid = textField_teacherid.getText();
+          String teacherid = textField_id.getText();
+          String position = textField_position.getText();
+          String tel = textField_tel.getText();
           Configreader reader = Configreader.reader("config.txt");
-          String tablename = reader.readItem("classcourse");
-          String classcourse_classid = reader.readItem("classcourse_classid");
-          String classcourse_semester = reader.readItem("classcourse_semester");
-          String classcourse_courseid = reader.readItem("classcourse_courseid");
-          String classcourse_teacherid = reader.readItem("classcourse_teacherid");
-          String sql = "update " + tablename + " set " + classcourse_classid + "=" + classid +
-              ", " + classcourse_semester + "=" + semester + 
-              ", " + classcourse_teacherid + "=" + teacherid +
-              " where " + classcourse_courseid + "=" + courseid;
-          String check_sql = "select * from " + tablename + " where " + classcourse_courseid + "=" + courseid;
+          String tablename = reader.readItem("teachers");
+          String teachers_id = reader.readItem("teachers_id");
+          String teachers_position = reader.readItem("teachers_position");
+          String teachers_tel = reader.readItem("teachers_tel");
+          String sql = "update " + tablename + " set ";
+          boolean flag = false;
+          if(!position.equals("")) {
+            flag = true;
+            sql += teachers_position + "='" + position + "'";
+          }
+          if(!tel.equals("")) {
+            if(flag) {
+              sql += ", ";
+            }
+            sql += teachers_tel + "='" + tel + "'";
+          }
+          sql += " where " + teachers_id + "=" + teacherid;
+          System.out.println(sql);
+          String check_sql = "select * from " + tablename + " where " + teachers_id + "=" + teacherid;
           
           Connection conn = Connector.getConnection();
           PreparedStatement psmt_check = conn.prepareStatement(check_sql);
@@ -119,29 +125,21 @@ public class Change_Teacher {
             psmt.executeUpdate();
             textField.setText("Success!");
           } else {
-            textField.setText("The course is not exist");
+            textField.setText("The teacher is not exist");
           }
         } catch (Exception exception){
           exception.printStackTrace();
         }
       }
     });
-    button.setBounds(20, 250, 107, 75);
+    button.setBounds(20, 197, 107, 75);
     frame.getContentPane().add(button);
     
     textField = new JTextField();
-    textField.setBounds(141, 250, 250, 75);
+    textField.setBounds(141, 197, 250, 75);
     frame.getContentPane().add(textField);
     textField.setColumns(10);
     
-    label_2 = new JLabel("修改后教师编号：");
-    label_2.setFont(new Font("微软雅黑", Font.PLAIN, 18));
-    label_2.setBounds(68, 190, 170, 32);
-    frame.getContentPane().add(label_2);
-    
-    textField_teacherid = new JTextField();
-    textField_teacherid.setColumns(10);
-    textField_teacherid.setBounds(215, 190, 108, 32);
-    frame.getContentPane().add(textField_teacherid);
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE );
   }
 }
